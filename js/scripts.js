93aba644091219c1dce51507741c79ce68ff083d
masha <mashanechmo@gmail.com>
2021-06-18 14:18:28 -0700

    add UI function that process user input

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,13 @@ Pizza.prototype.addPrice = function(){
   this.toppings.map(function(num){
     return i += 1;
   })
+
+
+
+
+  $(document).ready(function() {
+    $("form#pizzas").submit(function(event) {
+      event.preventDefault()
+      var size = $("input[name=size]:checked").val();
+      var selectedPizza = $("input[name=pizzaChoice]:checked").val();
+      var toppingArray = [];

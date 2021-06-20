605f731e18fff37c7ed8fdb03f8cca1b671fd655
masha <mashanechmo@gmail.com>
2021-06-20 16:00:21 -0700

    add css for body and h2

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
-function Pizza (toppings, size, type ){
+function Pizza(toppings, size, type) {
   this.toppings = toppings;
   this.size = size;
   this.type = type;
   this.cost = 0;
 };
 
-Pizza.prototype.addPrice = function(){
+Pizza.prototype.addPrice = function () {
   var i = 0;
-  this.toppings.map(function(num){
+  this.toppings.map(function (num) {
     return i += 1;
   })
   this.cost = i;
-  if (this.size === "large"){
+  if (this.size === "large") {
     console.log(this.cost)
     this.cost += 10;
-  } else if (this.size === "small"){
+  } else if (this.size === "small") {
     this.cost += 5;
   } else {
     alert("pick a size")
@@ -22,21 +22,20 @@ Pizza.prototype.addPrice = function(){
   return this.cost
 };
 
-  $(document).ready(function() {
-    $("form#pizzas").submit(function(event) {
-      event.preventDefault()
-      var size = $("input[name=size]:checked").val();
-      var selectedPizza = $("input[name=pizzaChoice]:checked").val();
-      var toppingArray = [];
-      $("#t:checked").each(function(){
-        toppingArray.push($(this).val());
-        console.log(toppingArray)
-      })
-      var myPizza = new Pizza (toppingArray, size, selectedPizza)
-      myPizza.addPrice()
-      $("#cart").text(myPizza.type)
-      $("#total").text(myPizza.cost)
-      console.log(myPizza)
+$(document).ready(function () {
+  $("form#pizzas").submit(function (event) {
+    event.preventDefault()
+    var size = $("input[name=size]:checked").val();
+    var selectedPizza = $("input[name=pizzaChoice]:checked").val();
+    var toppingArray = [];
+    $("#t:checked").each(function () {
+      toppingArray.push($(this).val());
+      console.log(toppingArray)
     })
+    var myPizza = new Pizza(toppingArray, size, selectedPizza)
+    myPizza.addPrice()
+    $("#cart").text(myPizza.type)
+    $("#total").text(myPizza.cost)
+    console.log(myPizza)
   })
-  
+})
